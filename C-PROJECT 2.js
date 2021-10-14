8e051b4adf46e482e6ed2c53402c9ac141edcc94
krithindeepan <84080581+krithindeepan@users.noreply.github.com>
2021-10-14 10:50:59 +0530

    Add files via upload

---
+++ C-PROJECT 2.js
@@ -0,0 +1,11 @@
+menu_list_array = ["Chicken Pizza"
+//add more items
+];
+document.getElementById("     ").innerHTML = htmldata;
+//give the appropirate id name as display_menu
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value
+// use the sort function as - menu_list_array.sort();
+htmldata=""
+}
